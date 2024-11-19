2650ce89307463f864411a2009a593d22ff2701a
hwang105814 <hwang105814@gmail.com>
2024-11-20 04:30:35 +0900

    Walk animation using blend tree

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Physics
+    Rigidbody2D playerRb;
+    //Animation
+    Animator playerAnimator;
+
     void Start()
     {
-        
+        //Physics
+        playerRb = GetComponent<Rigidbody2D>();
+        //Animation
+        playerAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        //Player move
+        Vector2 moveVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Debug.Log(moveVector);
+        playerRb.linearVelocity = moveVector.normalized*PlayerStat.Instance.walkSpeed;
+        playerAnimator.SetFloat("InputX", moveVector.x);
+        playerAnimator.SetFloat("InputY", moveVector.y);
     }
 }
---
+++ Assets/Scripts/Player/PlayerStat.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerStat
+{
+    private static PlayerStat instance;
+    public static PlayerStat Instance {  
+        get
+        {
+            if(instance == null)
+            {
+                instance = new PlayerStat();
+            }
+            return instance;
+        }
+    }
+    public float spearAtk = 2;
+    public float gunAtk = 3;
+
+    public float walkSpeed = 5;
+    public float runSpeed = 10;
+}
