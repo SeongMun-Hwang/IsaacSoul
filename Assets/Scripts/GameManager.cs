b5825607c6b05e486e7966afd670e1ca385cea17
hwang105814 <hwang105814@gmail.com>
2024-12-01 01:03:56 +0900

    Change navmesh surface bake option to volume

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,21 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
+public class Row
+{
+    public List<int> columns = new List<int>(3);
+}
+
 public class GameManager : MonoBehaviour
 {
-    public List<List<GameObject>> gameMap;
+    public List<Row> rows = new List<Row>(3);
+    public List<GameObject> roomsPrefab;
     public int roomNumber;
+    float horizontalDistance = 34;
+    float verticalDistance = 22;
 
     public void CreateMap()
     {
