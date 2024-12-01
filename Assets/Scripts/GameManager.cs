d91cde11bb19792b7280b86d1a3761df7caf3241
hwang105814 <hwang105814@gmail.com>
2024-12-01 19:37:54 +0900

    Add CreateMap function(tempo)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,24 +1,64 @@
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[Serializable]
-public class Row
+public class Room
 {
-    public List<int> columns = new List<int>(3);
+    public float posX;
+    public float posY;
+    public GameObject room;
 }
 
 public class GameManager : MonoBehaviour
 {
-    public List<Row> rows = new List<Row>(3);
     public List<GameObject> roomsPrefab;
+    public GameObject baseRoomPrefab;
     public int roomNumber;
     float horizontalDistance = 34;
     float verticalDistance = 22;
 
-    public void CreateMap()
+    private void Start()
     {
+        Room baseRoom = new Room();
+        baseRoom.posX = 0;
+        baseRoom.posY = 0;
+        baseRoom.room = Instantiate(baseRoomPrefab, transform.position, Quaternion.identity);
 
+        CreateMap(baseRoom);
+    }
+    public void CreateMap(Room baseRoom)
+    {
+        List<Room> createdRooms = new List<Room> { baseRoom };
+        Vector2[] directions = new Vector2[]
+        {
+            new Vector2(0, 1),
+            new Vector2(0, -1),
+            new Vector2(-1, 0),
+            new Vector2(1, 0)
+        };
+        for (int i = 0; i < roomNumber; i++)
+        {
+   
+            Room lastRoom = createdRooms[Random.Range(0, createdRooms.Count)];
+
+            Vector2 randomDirection = directions[Random.Range(0, directions.Length)];
+
+            float newX = lastRoom.posX + randomDirection.x * horizontalDistance;
+            float newY = lastRoom.posY + randomDirection.y * verticalDistance;
+
+            if (createdRooms.Exists(room => room.posX == newX && room.posY == newY))
+            {
+                i--;
+                continue;
+            }
+
+            Room newRoom = new Room();
+            newRoom.posX = newX;
+            newRoom.posY = newY;
+            GameObject randomRoomPrefab = roomsPrefab[Random.Range(0, roomsPrefab.Count)];
+            newRoom.room = Instantiate(randomRoomPrefab, new Vector3(newX, newY, 0), Quaternion.identity);
+
+            createdRooms.Add(newRoom);
+        }
     }
 }
